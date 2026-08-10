d90afa4def7679c6a2bb1498a35167fbde46bb30
LE <lele748916898@outlook.com>
2026-08-10 22:17:48 +0800

    增加实验功能偏好存储

--- Tests/GitMateCoreTests/TestMain.swift
+++ Tests/GitMateCoreTests/TestMain.swift
@@ -4,6 +4,7 @@ import Foundation
 struct GitMateCoreTestsMain {
     static func main() async {
         let tests = onboardingRouteTests
+            + experimentalFeaturePreferenceTests
             + repositoryWorkspaceModelTests
             + onboardingStateTests
             + githubDeviceFlowTests
